9c834f0961e2be3829b1fc196dd14646f1ae78c7
sergica97 <sergiocaballeroalejo@gmail.com>
2019-02-04 14:39:30 +0100

    Creamos las dos nuevas Activitys.

---
+++ app/src/main/java/com/example/practicat6/personas.java
@@ -0,0 +1,13 @@
+package com.example.practicat6;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class personas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_personas);
+    }
+}
