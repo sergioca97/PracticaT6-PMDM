1926e56f0238c1f30dfc902797159361556b85a8
sergica97 <sergiocaballeroalejo@gmail.com>
2019-02-06 21:08:37 +0100

    Realizamos una modificación para que el botón editar persona aparezca en la barra de la activity mediante un icono.

--- app/src/main/java/com/example/practicat6/personas.java
+++ app/src/main/java/com/example/practicat6/personas.java
@@ -1,26 +1,21 @@
 package com.example.practicat6;
 
 
-import android.content.Context;
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.content.pm.PackageManager;
-import android.net.Uri;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.view.menu.MenuBuilder;
 import android.view.ContextMenu;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.Toast;
-
-import static android.Manifest.permission.CALL_PHONE;
 
 public class personas extends AppCompatActivity
 {
-    private static final int MY_PERMISSIONS_REQUEST_CALL_PHONE =  1;
     ImageView imgPersona1, imgPersona2, imgPersona3, imgPersona4, imgPersona5, imgPersona6;
     SharedPreferences prefs;
     String telefono, correo;
@@ -103,59 +98,25 @@ public class personas extends AppCompatActivity
             }
         });
     }
+    @SuppressLint("RestrictedApi")
+    @Override
+    public boolean onCreateOptionsMenu(Menu editar)
+    {
+        getMenuInflater().inflate(R.menu.editar, editar);
+        MenuBuilder m = (MenuBuilder) editar;
+        m.setOptionalIconsVisible(true);
+        return true;
+    }
     public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
         super.onCreateContextMenu(menu, v, menuInfo);
-        getMenuInflater().inflate(R.menu.editarpersonas, menu);
+        getMenuInflater().inflate(R.menu.personas, menu);
     }
 
-    public boolean onContextItemSelected(MenuItem item)
+    public boolean onOptionsItemSelected(MenuItem item)
     {
-        String numero="";
-        String correos="";
-        prefs = getSharedPreferences("almacenarPersonas", Context.MODE_PRIVATE);
-
-        switch(item.getItemId())
-        {
-            case R.id.llamar:
-                numero = prefs.getString(telefono, null);
-                if(numero==null)
-                {
-                    Toast.makeText(this, R.string.toastTelefono, Toast.LENGTH_LONG).show();
-                }
-                else
-                {
-                    if (ContextCompat.checkSelfPermission(getApplicationContext(), CALL_PHONE) == PackageManager.PERMISSION_GRANTED)
-                    {
-                        Intent callIntent = new Intent(Intent.ACTION_CALL);
-                        callIntent.setData(Uri.parse(telefono));
-                        startActivity(callIntent);
-                    }
-                    else
-                    {
-                        requestPermissions(new String[]{CALL_PHONE}, 1);
-                    }
-                }
-                break;
-            case R.id.correo:
-                correos = prefs.getString(correo, null);
-                if(correos==null)
-                {
-                    Toast.makeText(this, R.string.toastCorreo, Toast.LENGTH_LONG).show();
-                }
-                else
-                {
-                    Intent emailIntent = new Intent(Intent.ACTION_SEND);
-                    emailIntent.setType("text/plain");
-                    emailIntent.putExtra(Intent.EXTRA_EMAIL, new String[]{correos});
-                    startActivity(emailIntent);
-                }
-                break;
-            case R.id.editar:
-                Intent intent2 = new Intent(this, editarDatos.class);
-                startActivity(intent2);
-                return false;
-        }
-        return super.onContextItemSelected(item);
+        Intent intento2 = new Intent(this, editarDatos.class);
+        startActivity(intento2);
+        return true;
     }
 }
 
