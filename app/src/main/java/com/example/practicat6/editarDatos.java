a2844399debbaa497f1a55706d68b59bdf18f377
sergica97 <sergiocaballeroalejo@gmail.com>
2019-02-06 18:59:13 +0100

    Añadimos las imágenes para que al entrar en editar y seleccionar una imagen a editar ponga las demás a invisible.

--- app/src/main/java/com/example/practicat6/editarDatos.java
+++ app/src/main/java/com/example/practicat6/editarDatos.java
@@ -5,13 +5,13 @@ import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 public class editarDatos extends AppCompatActivity
 {
+    ImageView imgPersona1, imgPersona2, imgPersona3, imgPersona4, imgPersona5, imgPersona6;
     EditText edtTelefono, edtCorreo;
     String numero = "", correo = "";
 
@@ -19,7 +19,12 @@ public class editarDatos extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editar_datos);
-
+        imgPersona1 = findViewById(R.id.imgPersona1);
+        imgPersona2 = findViewById(R.id.imgPersona2);
+        imgPersona3 = findViewById(R.id.imgPersona3);
+        imgPersona4 = findViewById(R.id.imgPersona4);
+        imgPersona5 = findViewById(R.id.imgPersona5);
+        imgPersona6 = findViewById(R.id.imgPersona6);
         edtTelefono = findViewById(R.id.edtTelefono);
         edtCorreo = findViewById(R.id.edtCorreo);
     }
@@ -30,44 +35,72 @@ public class editarDatos extends AppCompatActivity
         editor.putString(numero, edtTelefono.getText().toString());
         editor.putString(correo, edtCorreo.getText().toString());
         editor.commit();
-        Toast.makeText(this,"Campos actualizados correctamente." , Toast.LENGTH_SHORT).show();
+        Toast.makeText(this,R.string.toastCampos , Toast.LENGTH_SHORT).show();
     }
     public void persona1(View view)
     {
-        Toast.makeText(this,"Has seleccionado la persona 1" , Toast.LENGTH_SHORT).show();
-        numero="teléfono persona 1";
-        correo="correo persona 1";
+        Toast.makeText(this,R.string.toastPersona1 , Toast.LENGTH_SHORT).show();
+        imgPersona1.setVisibility(View.VISIBLE);
+        imgPersona2.setVisibility(View.INVISIBLE);
+        imgPersona3.setVisibility(View.INVISIBLE);
+        imgPersona4.setVisibility(View.INVISIBLE);
+        imgPersona5.setVisibility(View.INVISIBLE);
+        imgPersona6.setVisibility(View.INVISIBLE);
     }
     public void persona2(View view)
     {
-        Toast.makeText(this,"Has seleccionado la persona 2" , Toast.LENGTH_SHORT).show();
-        numero="teléfono persona 2";
-        correo="correo persona 2";
+        Toast.makeText(this,R.string.toastPersona2 , Toast.LENGTH_SHORT).show();
+        imgPersona1.setVisibility(View.INVISIBLE);
+        imgPersona2.setVisibility(View.VISIBLE);
+        imgPersona3.setVisibility(View.INVISIBLE);
+        imgPersona4.setVisibility(View.INVISIBLE);
+        imgPersona5.setVisibility(View.INVISIBLE);
+        imgPersona6.setVisibility(View.INVISIBLE);
+
 
     }
     public void persona3(View view)
     {
-        Toast.makeText(this,"Has seleccionado la persona 3" , Toast.LENGTH_SHORT).show();
-        numero="teléfono persona 3";
-        correo="correo persona 3";
+        Toast.makeText(this,R.string.toastPersona3 , Toast.LENGTH_SHORT).show();
+        imgPersona1.setVisibility(View.INVISIBLE);
+        imgPersona2.setVisibility(View.INVISIBLE);
+        imgPersona3.setVisibility(View.VISIBLE);
+        imgPersona4.setVisibility(View.INVISIBLE);
+        imgPersona5.setVisibility(View.INVISIBLE);
+        imgPersona6.setVisibility(View.INVISIBLE);
+
     }
     public void persona4(View view)
     {
-        Toast.makeText(this,"Has seleccionado la persona 4" , Toast.LENGTH_SHORT).show();
-        numero="teléfono persona 4";
-        correo="correo persona 4";
+        Toast.makeText(this,R.string.toastPersona4 , Toast.LENGTH_SHORT).show();
+        imgPersona1.setVisibility(View.INVISIBLE);
+        imgPersona2.setVisibility(View.INVISIBLE);
+        imgPersona3.setVisibility(View.INVISIBLE);
+        imgPersona4.setVisibility(View.VISIBLE);
+        imgPersona5.setVisibility(View.INVISIBLE);
+        imgPersona6.setVisibility(View.INVISIBLE);
     }
     public void persona5(View view)
     {
-        Toast.makeText(this,"Has seleccionado la persona 5" , Toast.LENGTH_SHORT).show();
-        numero="teléfono persona 5";
-        correo="correo persona 5";
+        Toast.makeText(this,R.string.toastPersona5 , Toast.LENGTH_SHORT).show();
+        imgPersona1.setVisibility(View.INVISIBLE);
+        imgPersona2.setVisibility(View.INVISIBLE);
+        imgPersona3.setVisibility(View.INVISIBLE);
+        imgPersona4.setVisibility(View.INVISIBLE);
+        imgPersona5.setVisibility(View.VISIBLE);
+        imgPersona6.setVisibility(View.INVISIBLE);
+
     }
     public void persona6(View view)
     {
-        Toast.makeText(this,"Has seleccionado la persona 6" , Toast.LENGTH_SHORT).show();
-        numero="teléfono persona 6";
-        correo="correo persona 6";
+        Toast.makeText(this,R.string.toastPersona6 , Toast.LENGTH_SHORT).show();
+        imgPersona1.setVisibility(View.INVISIBLE);
+        imgPersona2.setVisibility(View.INVISIBLE);
+        imgPersona3.setVisibility(View.INVISIBLE);
+        imgPersona4.setVisibility(View.INVISIBLE);
+        imgPersona5.setVisibility(View.INVISIBLE);
+        imgPersona6.setVisibility(View.VISIBLE);
+
 
     }
 }
